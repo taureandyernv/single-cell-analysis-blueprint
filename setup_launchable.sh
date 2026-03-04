d36adfd6f0976d0a967bcb91482eefbb6aa5a477
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2026-03-04 11:22:17 -0800

    test second kernel

---
+++ setup_launchable.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+uv --version
+# uv venv
+cd /notebooks
+uv pip install --system -r requirements.txt
+
+# Install new kernel
+python -m venv .venv
+source .venv/bin/activate
+
+# Install with all dependencies using uv for CUDA 13
+export PATH="$HOME/.local/bin:$PATH"
+uv sync --extra cuda13
+
+# Install Jupyter and JupyterLab
+uv pip install ipykernel
+mamba install -c cupy==14 numpy==2.2.6 -y
+
+# Create a Jupyter kernel for this environment
+uv run python -m ipykernel install --user --name=out-of-core --display-name "Out of Core Notebook"
+
+set -m
+# Start the primary process and put it in the background
+jupyter-lab --notebook-dir=/notebooks --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
