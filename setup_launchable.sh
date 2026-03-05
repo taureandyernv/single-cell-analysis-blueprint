10487dbb33a3c897630b6e964fe2c8e78140c2b7
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2026-03-04 18:35:34 -0800

    try prefix

--- setup_launchable.sh
+++ setup_launchable.sh
@@ -6,17 +6,19 @@ uv --version
 # uv venv
 cd /notebooks
 uv pip install --system -r requirements.txt
+mamba install -c conda-forge -c rapidsai cupy==14.0.1 numpy==2.2.6 -y
 
 # Install with all dependencies using uv for CUDA 13
-export PATH="$HOME/.local/bin:$PATH"
-uv sync --extra cuda13
+# export PATH="$HOME/.local/bin:$PATH"
+# uv sync --extra cuda13
 
 # Install Jupyter and JupyterLab
-mamba create --name test -c python=3.13 conda-forge cupy=14 numpy=2.2.6 -y
-
+#mamba create --name test -c conda-forge cupy=14.0.1 numpy=2.2.6 -y
+#conda activate test
 # Create a Jupyter kernel for this environment
-uv run python -m ipykernel install --prefix /opt/conda/ --name=out-of-core --display-name "Out of Core Notebook"
-
+#uv run python -m ipykernel install --prefix /opt/conda/ --name=out-of-core --display-name "Out of Core Notebook"
+#conda deactivate
 set -m
 # Start the primary process and put it in the background
+export CONDA_PREFIX=/opt/conda
 jupyter-lab --notebook-dir=/notebooks --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
